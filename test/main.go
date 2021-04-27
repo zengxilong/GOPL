b66f5675e9ef01dd8c8ab4036a3db3f04b0c206f
zengxilong <1637545108@qq.com>
2021-04-27 21:04:42 +0800

    ch4/Json related code

--- test/main.go
+++ test/main.go
@@ -16,4 +16,7 @@ func main() {
 	r, size := utf8.DecodeRuneInString("世界")
 	fmt.Printf("%d\t%c\n", size, r)
 	fmt.Println(utf8.RuneCountInString(s))
+
+	value := [6]int{1, 2, 3, 4, 5, 6}
+	fmt.Println(value[:0])
 }
